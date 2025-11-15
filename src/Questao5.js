08fed5a8b9deea21cf9503f333d0908325b29a2a
marcioflorenco-code <marcio.florenco@academico.ifpb.edu.br>
2025-11-14 23:56:13 -0300

    Update Questao5.js

--- src/Questao5.js
+++ src/Questao5.js
@@ -1,1 +1,23 @@
+const queryString = "categoria=eletronicos&preco=500,00&marca=samsung&avaliacao=4.5";
 
+function parseQueryString(query) {
+  const obj = {};
+
+  // Divide pelos parâmetros
+  const params = query.split("&");
+
+  for (let p of params) {
+    let [key, value] = p.split("=");
+
+  
+    if (!isNaN(value) && value.trim() !== "") {
+      value = Number(value);
+    }
+
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
+console.log(parseQueryString(queryString));
