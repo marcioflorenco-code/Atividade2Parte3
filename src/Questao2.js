99085dcdf23b00b7a533a267e268ab8d81808d0d
marcioflorenco-code <marcio.florenco@academico.ifpb.edu.br>
2025-11-14 21:41:41 -0300

    Update Questao2.js

--- src/Questao2.js
+++ src/Questao2.js
@@ -1,1 +1,32 @@
+function analisarTexto(texto) {
+  const palavras = texto
+    .toLowerCase()
+    .replace(/[.,!?;:]/g, "")   // remove pontuação
+    .split(/\s+/);               // separa por espaço
 
+  const totalPalavras = palavras.length;
+
+  const mapaFrequencia = {};
+
+  palavras.forEach(p => {
+    mapaFrequencia[p] = (mapaFrequencia[p] || 0) + 1;
+  });
+
+  const frequenciaPalavras = Object.keys(mapaFrequencia).map(p => ({
+    palavra: p,
+    frequencia: mapaFrequencia[p]
+  }));
+
+  const totalCaracteres = palavras.reduce((acc, p) => acc + p.length, 0);
+  const tamanhoMedioPalavras = totalCaracteres / totalPalavras;
+
+  return {
+    totalPalavras,
+    frequenciaPalavras,
+    tamanhoMedioPalavras: Number(tamanhoMedioPalavras.toFixed(2))
+  };
+}
+
+const texto = "JavaScript é uma linguagem de programação. JavaScript é versátil e JavaScript é poderoso.";
+
+console.log(analisarTexto(texto));
